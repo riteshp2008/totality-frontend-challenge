99d8027ba3fcea5cf1f4027751b47d7b68687fae
Ritesh <riteshpatel246@gmail.com>
2024-08-30 03:54:14 +0530

    refactor: update font size in Header component

--- app/main/Header.jsx
+++ app/main/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="bg-white shadow">
       <div className="container flex justify-between items-center py-4">
-        <div className="text-3xl font-semibold flex relative">
+        <div className="text-xl lg:text-3xl font-semibold flex relative">
           <Link href="/" className="flex items-center">
             <Shapes size={24} className="mr-2" />
             DreamStay
